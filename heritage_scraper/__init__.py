92012cd34495d06b6ff06df931e2f781e638f479
JuwonOh <13a71032776@gamil.com>
2019-08-27 21:34:36 +0900

    modify fatal error

---
+++ heritage_scraper/__init__.py
@@ -0,0 +1,7 @@
+from .parser import parse_page
+from .downloader import download
+from .utils import now
+from .utils import get_soup
+from .utils import user_dateformat
+from .scraper import yield_latest_article
+from .scraper import get_article_urls
